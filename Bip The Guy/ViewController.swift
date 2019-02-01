9735110ecc4c64cb196dfb9b8929edf01d2a3fe1
Katrina Liu <katrina@katrinas-macbook-pro.local>
2019-01-31 22:09:45 -0500

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Katrina Liu on 1/31/19.
+//  Copyright © 2019 Katrina Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
